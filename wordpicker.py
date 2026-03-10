3e4ef779d7b381c2ac453d24b3119c12005a578c
Alex Mak <alexcmak@gmail.com>
2026-03-09 20:02:08 -0500

    remove word option

--- wordpicker.py
+++ wordpicker.py
@@ -2,19 +2,33 @@ import random
 import twl
 import string 
 # A, B, C... Z Counts
-Counts = [9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1]
+Scrabble_Counts = [9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1]
+Scrabble_Words = list(twl.iterator())
 
-ALL_words = list(twl.iterator())
-
-i = 0
+Counts = []
+ALL_words = []
 All_Tiles = []
-for letter in string.ascii_uppercase:
-    for j in range(1, Counts[i]+1):
-       All_Tiles.append(letter)
-    i = i + 1
 
-#print(All_Tiles)
-#print(len(All_Tiles))
+chosen_words = []
+
+def init():
+    global Counts
+    global ALL_Tiles
+    global ALL_words
+    global Scrabble_Words
+    global Scrabble_Counts
+
+    Counts = Scrabble_Counts.copy()
+    ALL_words = Scrabble_Words.copy()
+
+    i = 0
+    All_Tiles = []
+    for letter in string.ascii_uppercase:
+     for j in range(1, Counts[i]+1):
+        All_Tiles.append(letter)
+     i = i + 1
+
+    reduce_bag()
 
 
 def show_bag(counts_arr):
@@ -96,6 +110,8 @@ def list_all_words():
 
 # put n into alphabet count
 def put_letter_in_bag(num, bag):
+    global All_Tiles
+
     nletter = All_Tiles[num]
     #print(f'{num} translates to {nletter}')
 
@@ -106,11 +122,31 @@ def put_letter_in_bag(num, bag):
             bag[index] = bag[index] + 1
         index = index + 1
 
+def remove_word(word):
+
+    global chosen_words
+
+    try:
+        chosen_words.remove(word)
+        print(f'{word} removed')
+        init()
+
+        for word in chosen_words:
+            use_word(word)
+
+    except:
+        print(f'{word} did not exist')
+
+
 def menu():
+    global chosen_words
+
     print('-------------------')
     print('l - list all words')
     print('s - show tiles')
     print('c - choose word')
+    if (len(chosen_words) > 0):
+        print('r - remove word')
     print('q - quit')
     user_input = input("Choice: ")
     return user_input
@@ -119,15 +155,15 @@ def menu():
 def main():
 
     global Counts
+    global chosen_words
 
+    init()
     show_bag(Counts)
 
     total = sum(Counts)
     print(f"total tiles {total}")
     print(f'{len(ALL_words)} words')
 
-
-    chosen_words = []
     # ------ main loop -----
     while(True):
 
@@ -144,6 +180,15 @@ def main():
                 if (use_word(word) == True):
                     chosen_words.append(word)
                     print(f"words chosen: {chosen_words}")
+            case 'r':
+
+                if (len(chosen_words) > 0):
+                    word = input ('Remove which word:')
+                    word = word.upper()
+                    remove_word(word)
+                    print(f"words chosen: {chosen_words}")
+                else:
+                    print('no chosen words yet.')
             case 'q':
                 break
             case _:
